4502b97c3313f0ae6f2c601dd4a71ad97de73e7a
Sheila Stephen <sheila.je@gmail.com>
2019-01-25 12:04:12 -0800

    add shopping cart component add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BsNavbarComponent,
-    HomeComponent
+    HomeComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     RouterModule.forRoot([
+      {path: "", component: HomeComponent},
+      {path: "shopping-cart", component: ShoppingCartComponent},
+    ])
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
